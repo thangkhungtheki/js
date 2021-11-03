7397256d0d94c210c3eabb2dbe0936ce2d73aae9
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2021-11-03 21:50:05 +0700

    Create testbackcat-v2.js

---
+++ testbackcat-v2.js
@@ -0,0 +1,17 @@
+let blackcat = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+   	  let sc = await fetch(linksrcjavphe + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {return resul.video;})
+  						.catch((err) => {return err});
+      if(sc == "false"){
+      
+      }else{
+      		const player = new OpenPlayerJS('video1');
+  			  player.src = {src: sc.toString()};
+  			  player.init();
+      }
+
+             }
+  	blackcat();
