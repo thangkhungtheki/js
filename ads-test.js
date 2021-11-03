5224edbe8e4bd80ede314534fbb20f06b94cb7c0
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2021-11-03 22:05:23 +0700

    Update ads-test.js

--- ads-test.js
+++ ads-test.js
@@ -1,1 +1,18 @@
-const player = new OpenPlayerJS('video1',{ads: {src: ''}});
+let blackcat = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+   	  let sc = await fetch(linksrcjavphe + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {return resul.video;})
+  						.catch((err) => {return err});
+      if(sc == "false"){
+          alert('Video bi loi')
+      }else{
+          const player = new OpenPlayerJS('video1',{ads: {src: 'https://www.videosprofitnetwork.com/watch.xml?key=3034aeb4f3e60e1d6936607404d13934'}});
+      		
+  			  player.src = {src: sc.toString()};
+  			  player.init();
+      }
+}
+  	blackcat();
+
