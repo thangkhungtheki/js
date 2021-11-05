b2e049198d712a27b1183fc62a97580f82daf642
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2021-11-05 15:15:12 +0700

    Update debackcat.js

--- debackcat.js
+++ debackcat.js
@@ -1,1 +1,12 @@
-
+let blackcat = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+   	  let sc = await fetch("https://apixxx21.herokuapp.com/http://hostvideossxx.ddns.net:3000/apigetlinkjavphe/" + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {return resul.video;})
+  						.catch((err) => {return err});			  
+			  const player = new OpenPlayerJS('video1');
+  			  player.src = {src: sc.toString()};
+  			  player.init();
+             }
+  	blackcat();
