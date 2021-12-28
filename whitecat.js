0e00d65d343bae786488d7973d103283716357a8
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2021-12-28 15:52:38 +0700

    Create whitecat.js

---
+++ whitecat.js
@@ -0,0 +1,12 @@
+let whitecat = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+   	  let sc = await fetch("https://apixxx21.herokuapp.com/http://hostvideossxx.ddns.net:4000/streamvideo/" + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {return resul.video;})
+  						.catch((err) => {return err});			  
+			  const player = new OpenPlayerJS('video1');
+  			  player.src = {src: sc.toString()};
+  			  player.init();
+             }
+  	blackcat();
