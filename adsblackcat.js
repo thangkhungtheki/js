5596649301545a6f59471a002ed2422f016278d0
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2022-01-08 21:20:51 +0700

    Create adsblackcat.js

---
+++ adsblackcat.js
@@ -0,0 +1,22 @@
+let adsblackcat = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+   	  let sc = await fetch("https://apixxx21.herokuapp.com/http://hostvideossxx.ddns.net:4000/streamvideo/" + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {return resul.video;})
+  						.catch((err) => {return err});			  
+			  const player = new OpenPlayerJS('video1');
+  			  if(sc == false){
+              	player.src = {src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'};
+              }else{
+                player.src = {src: sc};
+              }
+  			  
+  	player.init().then(() => {
+            player.loadAd('https://www.videosprofitnetwork.com/watch.xml?key=1891bde0ade47de9f5d99ecbedc1544f');
+
+          
+  	});
+
+             }
+  	adsblackcat();
