98feded4e87e25e71fb80295ff9c125d17260093
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2022-04-25 21:06:43 +0700

    Create blackcat_v2.js

---
+++ blackcat_v2.js
@@ -0,0 +1,22 @@
+let blackcat_v2 = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+   	  let sc = await fetch("https://apixxx21.herokuapp.com/http://hostvideossxx.ddns.net:3000/apigetlinkjavphe/" + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {return resul.video;})
+  						.catch((err) => {return err});			  
+			  const player = new OpenPlayerJS('video1');
+  			  if(sc == false){
+              	player.src = {src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'};
+              }else{
+                player.src = {src: sc};
+              }
+  			  
+  	player.init().then(() => {
+            player.loadAd('https://syndication.realsrv.com/splash.php?idzone=4541812');
+
+          
+  	});
+
+             }
+  	blackcat_v2();
