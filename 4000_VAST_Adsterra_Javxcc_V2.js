75e5e0e84bb1ffb213fd4d0283872f62854ea5a4
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2022-04-30 23:00:22 +0700

    Create 4000_VAST_Adsterra_Javxcc_V2.js

---
+++ 4000_VAST_Adsterra_Javxcc_V2.js
@@ -0,0 +1,22 @@
+let silvercat = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+   	  let sc = await fetch("https://hentaivipz.xyz/http://hostvideossxx.ddns.net:4000/streamvideo/" + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {return resul.video;})
+  						.catch((err) => {return err});			  
+			  const player = new OpenPlayerJS('video1');
+  			  if(sc == false){
+              	player.src = {src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'};
+              }else{
+                player.src = {src: sc};
+              }
+  			  
+  	player.init().then(() => {
+            player.loadAd('https://hentaivipz.xyz/https://www.videosprofitnetwork.com/watch.xml?key=4f753132c0c3804ad39265ca7ddedce3');
+
+          
+  	});
+
+             }
+  	silvercat();
