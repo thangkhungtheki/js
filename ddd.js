40b8845392ec06d5e89bc8d8beec762882983580
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2022-05-06 22:59:31 +0700

    Create ddd.js

---
+++ ddd.js
@@ -0,0 +1,21 @@
+let silvercat = async() => {
+  	  let value = await document.querySelector('code').attributes.name.value
+      var num = 0;
+      let element = await document.getElementById("video1")
+      if(element){
+	        await element.remove()
+      }
+   	  var sc = await fetch("https://hentaivipz.xyz/http://hostvideossxx.ddns.net:3000/apigetlinkjavphe/" + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {num=resul.length; return resul.result;})
+  						.catch((err) => {return err});
+      if(num < 200){
+	        await document.getElementById('jp').innerHTML = ("<iframe width='640' height='360' src='" + sc +"' frameborder='0' allowfullscreen></iframe>");
+      }else{
+	        await document.getElementById('jp').innerHTML = ("<video class='op-player op-player__media' id='video1' controls playsinline src='" + sc + "'>");
+	        const player = new OpenPlayerJS('video1');
+	        player.init();
+      }
+}
+  	silvercat();
