8bb859162d3f8d6a05791eca90fe7e659c84c883
thangkhungtheki <90571769+thangkhungtheki@users.noreply.github.com>
2022-05-07 13:54:44 +0700

    Create 4000_hamtto..js

---
+++ 4000_hamtto..js
@@ -0,0 +1,24 @@
+let silvercat = async() => {
+  	  var value = await document.querySelector('code').attributes.name.value
+      var num = 0;
+      let element = await document.getElementById("video1")
+      if(element){
+	        await element.remove()
+      }
+   	  var sc = await fetch("https://hentaivipz.xyz/http://hostvideossxx.ddns.net:4000/streamvideo/" + value)
+  						.then((res) => {return res.text()})
+  						.then((result) => {return JSON.parse(result)})
+  						.then((resul) => {num=resul.length; return resul.result;})
+  						.catch((err) => {return err});
+      console.log(num)
+      console.log(sc)
+  	if(num < 200){
+	        document.getElementById('hamto').innerHTML = "<iframe width='640' height='360' src='" + sc + "' frameborder='0' allowfullscreen></iframe>";
+      }else{
+	        document.getElementById('hamto').innerHTML = "<video class='op-player op-player__media' id='video1' controls playsinline src='" + sc + "'>";
+	        const player = new OpenPlayerJS('video1');
+	        player.init();
+      }
+      
+}
+  	silvercat();
